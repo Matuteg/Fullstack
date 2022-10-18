bbf2aa600410f49b14acdfc480c719d027601907
MatuteG <matias123@hotmail.com>
2022-10-18 18:36:16 -0300

    refresh

--- assets/js/app.js
+++ assets/js/app.js
@@ -29,3 +29,10 @@ scrollUp.addEventListener("click", () => {
     behavior: "smooth",
   });
 });
+//Reset scroll top
+
+history.scrollRestoration = "manual"
+
+$(window).on('beforeunload', function(){
+      $(window).scrollTop(0);
+});
